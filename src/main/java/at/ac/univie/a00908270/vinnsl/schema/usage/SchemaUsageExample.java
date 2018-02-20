69e67033d31f864b43971913e335228c307d9353
a00908270 <a00908270@unet.univie.ac.at>
2018-02-20 15:22:56 +0100

    XSD, javadoc, examples
    * added all VINNSL 2.0 XSD files
    * generate all XSD schemas with XJC
    * generate XML examples for schema files
    * generate javadoc
    * provide schema usage example

---
+++ src/main/java/at/ac/univie/a00908270/vinnsl/schema/usage/SchemaUsageExample.java
@@ -0,0 +1,21 @@
+/*
+package at.ac.univie.a00908270.vinnsl.schema.usage;
+
+import at.ac.univie.a00908270.vinnsl.schema.Definition;
+import at.ac.univie.a00908270.vinnsl.schema.ObjectFactory;
+
+public class SchemaUsage {
+	
+	public static void main(String... args) {
+		Definition def = new ObjectFactory().createDefinition();
+		def.setIdentifier("test");
+		
+		Definition.Data data = new ObjectFactory().createDefinitionData();
+		data.setDescription("This is a description");
+		
+		def.setData(data);
+	}
+	
+	
+}
+*/
